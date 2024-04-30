e027d3b9e9a105f23429b9642bd4c26441482d7f
sabeenfatima <sabeenfatimaofficial@gmail.com>
2024-04-30 11:13:17 +0500

    Gaussian NB v1.0

--- train.py
+++ train.py
@@ -1,12 +1,9 @@
 import pandas as pd 
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB 
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve
-from sklearn.model_selection import train_test_split
 import json
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -16,8 +13,8 @@ df = pd.read_csv("data_processed.csv")
 
 #### Get features ready to model! 
 y = df.pop("cons_general").to_numpy()
-y[y< 4] = 0
-y[y>= 4] = 1
+y[y < 4] = 0
+y[y >= 4] = 1
 
 X = df.to_numpy()
 X = preprocessing.scale(X) # Is standard
@@ -27,9 +24,8 @@ imp = SimpleImputer(missing_values=np.nan, strategy='mean')
 imp.fit(X)
 X = imp.transform(X)
 
-
-# Linear model
-clf = SVC()
+# Gaussian Naive Bayes Classifier model
+clf = GaussianNB()  # Use Gaussian Naive Bayes Classifier
 yhat = cross_val_predict(clf, X, y, cv=5)
 
 acc = np.mean(yhat==y)
@@ -39,7 +35,7 @@ sensitivity = tp / (tp + fn)
 
 # Now print to file
 with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc, "specificity": specificity, "sensitivity":sensitivity}, outfile)
+    json.dump({ "accuracy": acc, "specificity": specificity, "sensitivity":sensitivity}, outfile)
 
 # Let's visualize within several slices of the dataset
 score = yhat == y
